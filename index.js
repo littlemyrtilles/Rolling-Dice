3ec01c82660f7305f42dc2e63f13501516968bb7
Stephi <135713274+littlemyrtilles@users.noreply.github.com>
2023-07-11 21:39:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ document.querySelector("button").addEventListener("click", function () {
 const randomNumber1 = Math.floor(Math.random()*6) + 1;
 const randomNumber2 = Math.floor(Math.random()*6) + 1;
  
-document.querySelector(".img1").setAttribute("src", `/images/D${randomNumber1}.png`);
-document.querySelector(".img2").setAttribute("src", `/images/D${randomNumber2}.png`);
+document.querySelector(".img1").setAttribute("src", '/images/D${randomNumber1}.png');
+document.querySelector(".img2").setAttribute("src", '/images/D${randomNumber2}.png');
 
 if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "It's A Draw!";
